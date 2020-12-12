410c9ef0a2d454045d3cd2a2c19be35c2283aba2
George N Monsour <gnmonsour@gmail.com>
2020-12-12 13:10:32 -0500

    including configs other than dev for ci env

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '12167740165-22utsierb6ohmidgb099iem4kcthipu6.apps.googleusercontent.com',
+  googleClientSecret: 'ebI0xbHSFjotyA6_WIMvVhpt',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: 'travisCat',
+  redisUrl: 'redis://127.0.0.1:6379',
+};
